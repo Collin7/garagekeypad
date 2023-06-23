ea3f8a3188088c604a94f00ad894ce39eb144f79
collin <collin.dewitt@fnb.co.za>
2023-06-23 16:34:59 +0200

    Major refactor, and not useding timer library, I suspect caused issues

--- garagekeypad.ino
+++ garagekeypad.ino
@@ -1,5 +1,4 @@
 #include <Credentials.h>
-#include <SimpleTimer.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
@@ -8,61 +7,49 @@
 
 const char* DEVICE_NAME = "GARAGE KEYPAD";
 
-#define keypad_code_topic "garage/keypad/code"
+const char* keypad_code_topic = "garage/keypad/code";
 //These two are from the garage controller, used here to play the success tune on buzzer
-#define frontdoor_status_topic "garage/door/status/front"
-#define backdoor_status_topic "garage/door/status/back"
-#define restart_topic "garage/door/status/restart"
-#define unknown_code_topic "garage/keypad/code/error"
+const char* frontdoor_status_topic = "garage/door/status/front";
+const char* backdoor_status_topic = "garage/door/status/back";
+const char* restart_topic = "garage/door/status/restart";
+const char* unknown_code_topic = "garage/keypad/code/error";
 
-//This can be used to output the date the code was compiled
-const char compile_date[] = __DATE__ " " __TIME__;
-
-//Initialize Objects
-WiFiClient garageKeypad;
-SimpleTimer timer;
-int timerId;
-PubSubClient client(garageKeypad);
-
-String input = "";   // keypad input
-String strTopic;
-String strPayload;
+char input[8];  // keypad input
+int inputIndex = 0;
+unsigned long lastKeyPressTime = 0;
+const unsigned long NO_ACTIVITY_INPUT_TIMEOUT = 20000;  // Timeout 20 sec
 
 //Keyapad details
 const byte ROWS = 4;
 const byte COLS = 4;
 char keys[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
 };
 
-byte rowPins[ROWS] = {0, 4, 5, 16}; //D3 D2 D1 D0
-byte colPins[COLS] = {13, 12, 14, 2}; //D7 D6 D5 D4
-
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+byte rowPins[ROWS] = { 0, 4, 5, 16 };    //D3 D2 D1 D0
+byte colPins[COLS] = { 13, 12, 14, 2 };  //D7 D6 D5 D4
 
 //Tones - sound frequency and melodies
-int successFailTone[] = {261, 277, 294, 311, 330, 349, 370, 392, 415, 440};
+int successFailTone[] = { 261, 277, 294, 311, 330, 349, 370, 392, 415, 440 };
 int normalKeypressTone = 1760;
 int cancelTone = 3520;
 int errorTone = 370;
 
 const int SPEAKER_PIN = 15;
 
+//Initialize Objects
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+WiFiClient garageKeypad;
+PubSubClient client(garageKeypad);
+
 void setup() {
   Serial.begin(115200);
-  setup_wifi();
-
-  client.setServer(MQTT_SERVER, MQTT_PORT);
-  client.setCallback(callback); //callback is the function that gets called for a topic sub
-
-  ArduinoOTA.setHostname(DEVICE_NAME);
-  ArduinoOTA.begin();
-
-  timerId = timer.setInterval(20000, clearInput); //Clear input every 10 sec if not empty
-  timer.disable(timerId);
+  setupWiFi();
+  setupMQTT();
+  setupOTA();
 }
 
 void loop() {
@@ -70,34 +57,26 @@ void loop() {
   if (!client.connected()) {
     reconnect();
   }
-  client.loop(); //the mqtt function that processes MQTT messages
+  client.loop();  //the mqtt function that processes MQTT messages
   ArduinoOTA.handle();
   readKeypadPresses();
-  timer.run();
-}
-
-void clearInput() {
-  if (input != "") {
-    input = "";
-    cancelSoundEffect();
-    timer.disable(timerId);
-  }
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  //Handle mqtt messages received my this NodeMCU
+
   payload[length] = '\0';
-  strTopic = String((char*)topic);
-  if (strTopic == frontdoor_status_topic || strTopic == backdoor_status_topic) {
-    String doorState = String((char*)payload);
-    if (doorState == "open" || doorState == "closed") {
+  char* strTopic = topic;
+
+  if (strcmp(strTopic, frontdoor_status_topic) == 0 || strcmp(strTopic, backdoor_status_topic) == 0) {
+    char* doorState = (char*)payload;
+    if (strcmp(doorState, "open") == 0 || strcmp(doorState, "closed") == 0) {
       successTone();
     } else {
       failTone();
     }
-  } else if (strTopic == unknown_code_topic){
+  } else if (strcmp(strTopic, unknown_code_topic) == 0) {
     failTone();
-  }else if (strTopic == restart_topic) {
+  } else if (strcmp(strTopic, restart_topic) == 0) {
     restartESP();
   }
 }
@@ -106,77 +85,45 @@ void readKeypadPresses() {
 
   char key = keypad.getKey();
 
-  if (key) {
-    timer.enable(timerId);
-
-    if (key != 'C' && key != '#') {
-      timer.restartTimer(timerId);
-      buttonPressSoundEffect();
-      input += key;
+  if (key == NO_KEY) {
+    if (millis() - lastKeyPressTime >= NO_ACTIVITY_INPUT_TIMEOUT) {
+      clearInput();
     }
+    return;
+  }
 
-    if (key == 'C') {
-      cancelSoundEffect();
-      input = "";
-      timer.disable(timerId);
+  lastKeyPressTime = millis();
+
+  if (key != 'C' && key != '#') {
+
+    buttonPressSoundEffect();
+    if (inputIndex < sizeof(input) - 1) {
+      input[inputIndex] = key;
+      inputIndex++;
+      input[inputIndex] = '\0';
     }
+  }
 
-    if (key == '#') {
-      buttonPressSoundEffect();
-      client.publish(keypad_code_topic, String(input).c_str());
-      input = "";
-      timer.disable(timerId);
+  if (key == 'C') {
+    clearInput();
+  }
+
+  if (key == '#') {
+    buttonPressSoundEffect();
+    //Only send if payload not empty
+    if (input[0] != '\0') {
+      client.publish(keypad_code_topic, input);
+      inputIndex = 0;
+      input[0] = '\0';
     }
   }
 }
 
-void setup_wifi() {
-
-  delay(10);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(WIFI_SSID);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println("MAC: ");
-  Serial.println(WiFi.macAddress());
-}
-
-void reconnect() {
-  //Reconnect to Wifi and to MQTT. If Wifi is already connected, then autoconnect doesn't do anything.
-
-  int retries = 0;
-  while (!client.connected()) {
-    if (retries < 15) {
-      Serial.print("Attempting MQTT connection...");
-      //Attempt to connect
-      if (client.connect(DEVICE_NAME, MQTT_USERNAME, MQTT_PASSWORD)) {
-        Serial.println("connected");
-        client.subscribe("garage/door/status/#");
-        client.subscribe("garage/keypad/code/error");
-      } else {
-        Serial.print("failed, rc=");
-        Serial.print(client.state());
-        Serial.println(" try again in 5 seconds");
-        retries++;
-        // Wait 5 seconds before retrying
-        delay(5000);
-      }
-    }
-    if (retries > 14) {
-      ESP.restart();
-    }
+void clearInput() {
+  cancelSoundEffect();
+  if (input[0] != '\0') {
+    inputIndex = 0;
+    input[0] = '\0';
   }
 }
 
@@ -210,6 +157,72 @@ void cancelSoundEffect() {
   tone(SPEAKER_PIN, cancelTone);
   delay(100);
   noTone(SPEAKER_PIN);
+}
+
+void setupWiFi() {
+
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  unsigned long startTime = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+
+    // Timeout if connection takes to long (15 seconds)
+    if (millis() - startTime > 15000) {
+      restartESP();
+    }
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("MAC: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void setupMQTT() {
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(callback);  // callback is the function that gets called for a topic subscription
+
+  Serial.println("Attempting MQTT connection...");
+  if (client.connect(DEVICE_NAME, MQTT_USERNAME, MQTT_PASSWORD)) {
+    Serial.println("Connected to MQTT broker");
+    subscribeMQTTTopics();
+  } else {
+    Serial.print("Failed to connect to MQTT broker. RC=");
+    Serial.println(client.state());
+    Serial.println("Restarting ESP...");
+    restartESP();
+  }
+}
+
+void subscribeMQTTTopics() {
+  client.subscribe("garage/door/status/#");
+  client.subscribe("garage/keypad/code/error");
+}
+
+void setupOTA() {
+  ArduinoOTA.setHostname(DEVICE_NAME);
+  ArduinoOTA.begin();
+}
+
+void reconnect() {
+
+  // Reconnect to WiFi and MQTT
+  if (WiFi.status() != WL_CONNECTED) {
+    setupWiFi();
+  }
+
+  // Check if MQTT connection is not established
+  if (!client.connected()) {
+    setupMQTT();
+  }
 }
 
 void restartESP() {
